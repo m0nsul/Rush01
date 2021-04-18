86eefe18117b53a219c845e29eb73f6b09d82391
Juanmi <juanmiguel.solanojimenez@telefonica.com>
2021-04-18 03:14:48 +0200

    new version checking all posible values for rows and views

--- ex00/main.c
+++ ex00/main.c
@@ -1,14 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define SIZE 4   
+#define SIZE 4
+
+int array1[] = {1,2,3,4}; //array can be {1,1,2,3} for example also
+int array2[SIZE];
+int fixed[SIZE];
+
+
+int	ft_strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i++])
+		;
+	return (i--);
+}
+
+int	ft_is_valid_argv_char(char c)
+{
+	if (c == ' ')
+		return (1);
+	else if ((c >= '1') && (c <= '4'))
+		return (1);
+	return (0);
+}
+
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		ft_atoi(char nbr)
+{
+	int n;
+
+	n = nbr - ('0');
+	return (n);
+}
+
+
+int	ft_viewscheck(char *views_str, int *views_int)
+{
+	int ci;
+	int cs;
+
+	ci = 1;
+	cs = 1;
+	views_int[0] = ft_atoi(views_str[0]);
+	while (views_str[cs] != '\0')
+	{
+		if (views_str[cs] == ' ' &&
+				views_str[cs + 1] >= '1' && views_str[cs + 1] <= '4')
+		{
+			views_int[ci] = ft_atoi(views_str[cs + 1]);
+			cs += 2;
+			ci++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
 
 int	ft_int_is_in_line (int a, int *line)
 {
 	int	count;
 
 	count = -1;
-	while (++count < 4)
+	while (++count < SIZE)
 	{
 		printf("\ncount: %d\tvalue: %d", count, line[count]);
 		if (line[count] == a)
@@ -22,7 +97,7 @@ int	ft_line_is_correct (int *line)
 	int	count;
 
 	count = 0;
-	while (count++ < 4)
+	while (count++ < SIZE)
 	{
 		printf("\ncount: %d\tft_int_is_in_line: %d", count, ft_int_is_in_line (count, line));
 		if (!(ft_int_is_in_line (count, line)))
@@ -30,11 +105,6 @@ int	ft_line_is_correct (int *line)
 	}
 	return (1);
 }
-
-// void	ft_print_matrix (*arr)
-// {
-
-// }
 
 int	ft_calculate_max_posibilities (void)
 {
@@ -98,86 +168,135 @@ int	ft_arr_is_in_posib (int *arr, int **posib)
 			}
 			j++;
 		}
-		if (j == 4)
+		if (j == SIZE)
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-
-void	ft_generate_all_posibilities (int **arr)
+void print_array(int array[], int size)
 {
-	int	max_posib;
-	int	i;
-	int	j;
-	int	c;
-
-	max_posib = ft_calculate_max_posibilities();
-
-	i = 0;
-	while (i < max_posib)
-	{
-		j = 0;
-		while (j < SIZE)
-		{
-			c = 0;
-			while (c++ < SIZE)
-			{
-				if (!(ft_int_is_in_array (c, arr[i])))
-				{
-					arr[i][j] = c;
-					break;
-				}
-			}
-			j++;
-		}
-		i++;
-	}
-
-	i = 0;
-	while (i < max_posib)
-	{
-		j = 0;
-		while (j < SIZE)
-		{
-			printf("%d ", arr[i][j]);
-			j++;
-		}
-		printf("\n");
-		i++;
-	}
+    for(int k = 0; k < size; k++) printf("%d ",array[k]);
 }
 
-
-int next_comb(int comb[], int k, int n) {
-	int i = k - 1;
-	++comb[i];
-	while ((i >= 0) && (comb[i] >= n - k + 1 + i)) {
-		--i;
-		++comb[i];
-	}
-
-	if (comb[0] > n - k) /* Combination (n-k, n-k+1, ..., n) reached */
-		return 0; /* No more combinations can be generated */
-
-	/* comb now looks like (..., x, n, n, n, ..., n).
-	Turn it into (..., x, x + 1, x + 2, ...) */
-	for (i = i + 1; i < k; ++i)
-		comb[i] = comb[i - 1] + 1;
-
-	return 1;
+int is_present_in_fixed(int array[], int size, int element )
+{
+    for(int k =0; k<size;k++)
+        if(element == array[k]) return 1;
+    return 0;
 }
 
-void printc(int comb[], int k) {
-	printf("{");
+void permWithRep(int **arr, int array2[], int fixed_index[], int size , int level, int posib){
+	int array3[SIZE];
 	int i;
-	for (i = 0; i < k; ++i)
-		printf("%d, ", comb[i] + 1);
-	printf("\b\b}\n");
+	int j;
+	int k;
+	int	max_posib;
+
+	i = -1;
+	while (++i < SIZE)
+		array3[i] = i + 1;
+
+    if(level == size) 
+	{
+		max_posib = ft_calculate_max_posibilities();
+		i = -1;
+		while (i++ < max_posib)
+			if (arr[i][0] == 0)
+			{
+				j = -1;
+				while (++j < SIZE)
+					arr[i][j] = array2[j];
+				break;
+			}
+		return ; 
+	}
+	k = -1;
+	while (++k < size)
+        if(!is_present_in_fixed(fixed_index,level,k))
+		{
+            fixed_index[level] = k;
+            array2[level] = array3[k];
+            permWithRep(arr, array2 ,fixed_index,size,level+1, posib);
+        }
 }
 
-int	main (void)
+int	ft_calculate_row_view_left (int *row)
+{
+	int	i;
+	int j;
+	int views;
+	int view;
+	views = 1;
+	i = 1;
+	while (i < SIZE)
+	{
+		j = i - 1;
+		view = 0;
+		while (j >= 0)
+			if (row[i] > row[j--])
+				view++;
+			j--;
+		if (view == i)
+			views++;
+		i++;
+	}
+	return views;
+}
+
+int	ft_calculate_row_view_right (int *row)
+{
+	int	i;
+	int j;
+	int views;
+	int view;
+	
+	views = 1;
+	i = SIZE - 2;
+	while (i >= 0)
+	{
+		j = i + 1;
+		view = 0;
+		while (j < SIZE)
+			if (row[i] > row[j++])
+				view++;
+		if (view == SIZE - i - 1)
+			views++;
+		i--;
+	}
+	return views;
+}
+
+
+
+// int	*ft_validate_arg_views (char *str) 
+// {
+
+// }
+
+int	check_main (int argc, char **argv)
+{
+	if (argc != 2) 
+	{
+        ft_putstr("Error: Wrong params number !\n");
+        return (0);
+    }
+	else if (ft_strlen(argv[1]) != 32)
+	{
+        ft_putstr("Error: Wrong point of view string format !\n");
+        return (0);
+    }
+	// else if (ft_validate_arg_views(argv[1]))
+	// {
+	// 	ft_putstring("Error: Wrong number of point of views  !\n");
+    //     return (0);
+	// }
+
+	return (1);
+}
+
+int	main (int argc, char **argv)
 {
 	int	filas;
 	int	columnas;
@@ -186,51 +305,51 @@ int	main (void)
 	int	c;
 	int	**arr;
 	int	max_posib;
+	int int_views[SIZE * SIZE];
 
 
+	check_main (argc, argv);
+	ft_viewscheck (argv[1], int_views);
 	
 
+	//print_array(int_views, SIZE * SIZE - 1);
 
 
 	max_posib = ft_calculate_max_posibilities();
-	i = 0;
 	arr = (int **) malloc (max_posib * sizeof (int *));
+	i = 0;
 	while (i < max_posib)
 	{
 		arr[i] = (int *) malloc (SIZE * sizeof(int));
 		i++;
 	}
+	permWithRep (arr, array1, fixed, SIZE, 0, 0);
 	i = 0;
 	while (i < max_posib)
 	{
-		j = 0;
-		while (j < SIZE)
+		print_array(arr[i], SIZE);
+		printf ("\tL %d ", ft_calculate_row_view_left (arr[i]));
+		printf ("R %d ", ft_calculate_row_view_right (arr[i]));
+		j=0;
+		while (j++ < SIZE)
 		{
-			c = 0;
-			while (c++ <= SIZE)	
-			{
-				arr[i][j] = 0;
-			}
-			j++;
+			printf ("\tL%d (%d)  ", j - 1, int_views[SIZE * 2 + j - 1]);
+			printf ("R%d (%d)", j - 1, int_views[SIZE * 3 + j - 1]);
+			if (ft_calculate_row_view_left (arr[i]) == int_views[SIZE * 2 + j - 1] && ft_calculate_row_view_right (arr[i]) == int_views[SIZE * 3 + j - 1])
+				printf (" OK\t");
+			else
+				printf ("   \t");
 		}
+		printf ("\n");
 		i++;
 	}
-	ft_generate_all_posibilities (arr);
 
 
 
-	int arr1[4] = {1,2,3,4};
-	int arr2[4] = {1,3,3,4};
-	printf("\n--------- %d --------\n", i );
-	//printf("%d", ft_arr_is_in_posib (arr2, arr));
-	//printf("%d", ft_arrays_are_equal (arr1, arr2));
 
 
 
-	while (next_comb(arr1, SIZE, SIZE))
-		printc(arr1, SIZE);
-
-	i = 0;
+	// i = 0;
 	// while (i < max_posib)
 	// {
 	// 	j = 0;
--- ex00/permutationprint.c
+++ ex00/permutationprint.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int array1[] = {1, 2, 3, 4}; //array can be {1,1,2,3} for example also
-int array2[4];
-int fixed[4];
+#define SIZE 4
+
+int array1[] = {1,2,3,4}; //array can be {1,1,2,3} for example also
+int array2[SIZE];
+int fixed[SIZE];
 
 void print_array(int array[], int size){
-    for(int k = 0; k < size; k++)printf("%d ",array[k]);
-    printf("\n");
+    for(int k = 0; k < size; k++) printf("%d ",array[k]);
+    //printf("\n");
 }
 
 int is_present_in_fixed(int array[], int size, int element ){
@@ -15,16 +17,37 @@ int is_present_in_fixed(int array[], int size, int element ){
     return 0;
 }
 
-void permWithRep(int array1[], int array2[],int fixed_index[], int size , int level){
+void permWithRep(int array2[], int fixed_index[], int size , int level){
+	int array3[SIZE];
+	int i;
 
-    if(level == size) return print_array(array2,size);
-
+	i = 0;
+	while (i < SIZE)
+	{
+		array3[i] = i + 1;
+		i++;
+	}
+	print_array(array3, SIZE);
+	printf("\n________________________\n");
+	printf("level: \t%d\t", level);
+	printf("Array 1:\t");
+	print_array(array3, SIZE);
+	printf("Array 2:\t");
+	print_array(array2, SIZE);
+	printf("Fixed  :\t");
+	print_array(fixed_index, SIZE);
+    if(level == size) 
+	{
+		printf("Encontrado:\t");
+		print_array(array2,size);
+		return ; // print_array(array2,size);
+	}
     for(int k = 0; k< size; k++){
-
+		printf("\nisPresentInfixed: %d\tk: %d", is_present_in_fixed(fixed_index,level,k), k);
         if(!is_present_in_fixed(fixed_index,level,k)){
             fixed_index[level] = k;
-            array2[level] = array1[k];
-            permWithRep(array1,array2,fixed_index,size,level+1);
+            array2[level] = array3[k];
+            permWithRep(array2,fixed_index,size,level+1);
         }
 
     }
@@ -32,7 +55,31 @@ void permWithRep(int array1[], int array2[],int fixed_index[], int size , int le
 
 int main()
 {
-    int len = sizeof(array1)/sizeof(int);
-    permWithRep (array1, array2, fixed, len, 0);
+	int	**arr;
+	int max_posib;
+	int	i;
+	int	j;
+
+	max_posib = ft_calculate_max_posibilities();
+	i = 0;
+	arr = (int **) malloc (max_posib * sizeof (int *));
+	while (i < max_posib)
+	{
+		arr[i] = (int *) malloc (SIZE * sizeof(int));
+		i++;
+	}
+
+	while (i < max_posib)
+	{
+		permWithRep (arr[i], fixed, SIZE, 0);
+		i++;
+	}
+
+    
+    permWithRep (array2, fixed, SIZE, 0);
+	printf("-------Array2------\n");
+	print_array(array2, sizeof(array2)/sizeof(int));
+	printf("-------Fixed------\n");
+	print_array(array2, sizeof(array2)/sizeof(int));
     return 0;
 }
